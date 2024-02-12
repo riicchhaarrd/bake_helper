a1cd7552a59b7525bfd70243cb07aa0b0912bb23
Richard <5922568+riicchhaarrd@users.noreply.github.com>
2024-02-12 02:40:33 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,23 +1,29 @@
+bl_info = {
+	"name": "Bake Helper",
+	"blender": (2, 80, 0),
+	"category": "Object",
+}
+
 import bpy
 from bpy.types import (Panel, Operator)
 from bpy.props import (StringProperty, IntProperty,
-                       PointerProperty,
-                       )
-                       
+					   PointerProperty,
+					   )
+					   
 from bpy.types import (Panel,
-                       PropertyGroup,
-                       )
+					   PropertyGroup,
+					   )
 
 # ------------------------------------------------------------------------
-#    Operators
+#	 Operators
 # ------------------------------------------------------------------------
 
 class Operator1(Operator):
-    """Operator"""
-    bl_idname = "object.operator1"
-    bl_label = ""
+	"""Operator"""
+	bl_idname = "object.operator1"
+	bl_label = ""
 
-    def execute(self, context):
+	def execute(self, context):
 		def setup_mesh(ob):
 			obj = bpy.context.active_object
 			new_mat = bpy.data.materials.new(name="NewMaterial")
@@ -39,7 +45,7 @@ class Operator1(Operator):
 			principled_node.location = (-200, 0)  # Adjust the location if needed
 
 			image_node = nodes.new('ShaderNodeTexImage')
-			image_node.location = (-600, 0)  # Adjust the location if needed
+			image_node.location = (-600, 0)	 # Adjust the location if needed
 
 			empty_image = bpy.data.images.new(name="EmptyImage", width=512, height=512)
 
@@ -48,7 +54,7 @@ class Operator1(Operator):
 			#links.new(image_node.outputs[0], principled_node.inputs['Base Color'])
 
 			output_node = nodes.new('ShaderNodeOutputMaterial')
-			output_node.location = (200, 0)  # Adjust the location if needed
+			output_node.location = (200, 0)	 # Adjust the location if needed
 
 			links.new(principled_node.outputs['BSDF'], output_node.inputs['Surface'])
 			
@@ -71,14 +77,14 @@ class Operator1(Operator):
 					obj.select_set(True)
 					setup_mesh(obj)
 
-        return {'FINISHED'}
+		return {'FINISHED'}
 		
 class Operator2(Operator):
-    """Operator"""
-    bl_idname = "object.operator2"
-    bl_label = ""
+	"""Operator"""
+	bl_idname = "object.operator2"
+	bl_label = ""
 
-    def execute(self, context):
+	def execute(self, context):
 		for obj in bpy.context.scene.objects:
 			if obj.type == 'MESH':
 				for slot in obj.material_slots:
@@ -110,14 +116,14 @@ class Operator2(Operator):
 									links.remove(link)
 							links.new(image_node.outputs[0], output_node.inputs[0])
 
-        return {'FINISHED'}
+		return {'FINISHED'}
 		
 class Operator3(Operator):
-    """Operator"""
-    bl_idname = "object.operator3"
-    bl_label = ""
+	"""Operator"""
+	bl_idname = "object.operator3"
+	bl_label = ""
 
-    def execute(self, context):
+	def execute(self, context):
 		for obj in bpy.context.scene.objects:
 			if obj.type == 'MESH':
 				# Iterate through all materials of the object
@@ -145,88 +151,88 @@ class Operator3(Operator):
 								links.remove(link)
 							links.new(principled_node.outputs[0], output_node.inputs[0])
 
-        return {'FINISHED'}
+		return {'FINISHED'}
 
 # ------------------------------------------------------------------------
-#    Panel in Object Mode
+#	 Panel in Object Mode
 # ------------------------------------------------------------------------
 
 class MyProperties(PropertyGroup):
 
-    image_resolution: IntProperty(
-        name="Bake image resolution",
-        description=":",
-        default=1024,
-        )
+	image_resolution: IntProperty(
+		name="Bake image resolution",
+		description=":",
+		default=1024,
+		)
 
-    #bar: StringProperty(
-    #    name="Bar",
-    #    description=":",
-    #    default="",
-    #    maxlen=1024,
-    #    )
+	#bar: StringProperty(
+	#	 name="Bar",
+	#	 description=":",
+	#	 default="",
+	#	 maxlen=1024,
+	#	 )
 		
 class OBJECT_PT_CustomPanel(Panel):
-    bl_idname = "object.custom_panel"
-    bl_label = "Bake Helper"
-    bl_space_type = "VIEW_3D"   
-    bl_region_type = "UI"
-    bl_category = "Tools"
-    bl_context = "objectmode"   
+	bl_idname = "object.custom_panel"
+	bl_label = "Bake Helper"
+	bl_space_type = "VIEW_3D"	
+	bl_region_type = "UI"
+	bl_category = "Tools"
+	bl_context = "objectmode"	
 
 
-    @classmethod
-    def poll(self,context):
-        return context.object is not None
+	@classmethod
+	def poll(self,context):
+		return context.object is not None
 
-    def draw(self, context):
-        layout = self.layout
-        scene = context.scene
-        mytool = scene.my_tool
-        obj = context.object
+	def draw(self, context):
+		layout = self.layout
+		scene = context.scene
+		mytool = scene.my_tool
+		obj = context.object
 
-        layout.label(text="Properties:")
+		layout.label(text="Properties:")
 
-        layout.prop(mytool, "image_resolution")
-        #col = layout.column(align=True)
-        #row = col.row(align=True)
-        #row.prop(obj, "show_name", toggle=True, icon="FILE_FONT")
-        #row.prop(obj, "show_wire", toggle=True, text="Wireframe", icon="SHADING_WIRE")
-        #col.prop(obj, "show_all_edges", toggle=True, text="Show all Edges", icon="MOD_EDGESPLIT")
-        layout.separator()
+		layout.prop(mytool, "image_resolution")
+		#col = layout.column(align=True)
+		#row = col.row(align=True)
+		#row.prop(obj, "show_name", toggle=True, icon="FILE_FONT")
+		#row.prop(obj, "show_wire", toggle=True, text="Wireframe", icon="SHADING_WIRE")
+		#col.prop(obj, "show_all_edges", toggle=True, text="Show all Edges", icon="MOD_EDGESPLIT")
+		layout.separator()
 
-        layout.label(text="Operators:")
+		layout.label(text="Operators:")
 
-        col = layout.column(align=True)
-        col.operator(Operator1.bl_idname, text="Add image to meshes", icon="CONSOLE")
-        col.operator(Operator2.bl_idname, text="Material output: Baked image", icon="CONSOLE")
-        col.operator(Operator3.bl_idname, text="Material output: Principled BSDF node", icon="CONSOLE")
+		col = layout.column(align=True)
+		col.operator(Operator1.bl_idname, text="Add image to meshes", icon="CONSOLE")
+		col.operator(Operator2.bl_idname, text="Material output: Baked image", icon="CONSOLE")
+		col.operator(Operator3.bl_idname, text="Material output: Principled BSDF node", icon="CONSOLE")
 
-        layout.separator()
+		layout.separator()
 
 # ------------------------------------------------------------------------
-#    Registration
+#	 Registration
 # ------------------------------------------------------------------------
 
 classes = (
-    MyProperties,
-    OBJECT_PT_CustomPanel,
+	MyProperties,
+	OBJECT_PT_CustomPanel,
 	Operator1,
 	Operator2,
 	Operator3
 )
 def register():
-    from bpy.utils import register_class
-    for cls in classes:
-        register_class(cls)
+	from bpy.utils import register_class
+	for cls in classes:
+		register_class(cls)
 
-    bpy.types.Scene.my_tool = PointerProperty(type=MyProperties)
+	bpy.types.Scene.my_tool = PointerProperty(type=MyProperties)
 
 def unregister():
-    from bpy.utils import unregister_class
-    for cls in reversed(classes):
-        unregister_class(cls)
-    del bpy.types.Scene.my_tool
+	from bpy.utils import unregister_class
+	for cls in reversed(classes):
+		unregister_class(cls)
+	del bpy.types.Scene.my_tool
 
 if __name__ == "__main__":
-    register()
+	register()
